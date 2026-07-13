84c29e5bd8cf577986dc082abc3346055cf7ac3f
MihirShahane <mihirshahane18@gmail.com>
2026-07-13 16:58:48 +0530

    Add files via upload

---
+++ image_alignment.py
@@ -0,0 +1,7 @@
+# This file covers all the image alignment techniques.
+
+print("Hello World")
+
+"""The underlying technique or phenomenon is Homography which transfers a square into any quadrilateral
+    We take 4 points on the scanned image and correspond them to the actual image to get a square image."""
+
